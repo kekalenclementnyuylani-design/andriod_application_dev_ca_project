8f669bc8913d13b368934c0fcf600fbd02b23793
237clement <kurtzclemente5@gmail.com>
2026-04-13 21:35:50 +0100

    initial commit

---
+++ smart_anti_theft_alarm/android/app/src/main/kotlin/com/example/smart_anti_theft_alarm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.smart_anti_theft_alarm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ smart_anti_theft_alarm/lib/services/sensor_service.dart
@@ -0,0 +1,40 @@
+﻿import 'dart:async';
+import 'dart:math';
+import 'package:sensors_plus/sensors_plus.dart';
+import '../utils/constants.dart';
+
+class SensorService {
+  StreamSubscription? _accelerometerSubscription;
+  double _baseX = 0, _baseY = 0, _baseZ = 0;
+  bool _baselineCaptured = false;
+  Function? onMotionDetected;
+
+  void startMonitoring() {
+    Future.delayed(const Duration(seconds: 1), () {
+      _accelerometerSubscription =
+          accelerometerEventStream().listen((AccelerometerEvent event) {
+        if (!_baselineCaptured) {
+          _baseX = event.x;
+          _baseY = event.y;
+          _baseZ = event.z;
+          _baselineCaptured = true;
+        } else {
+          double delta = sqrt(
+            pow(event.x - _baseX, 2) +
+            pow(event.y - _baseY, 2) +
+            pow(event.z - _baseZ, 2),
+          );
+          if (delta > AppConstants.movementThreshold) {
+            onMotionDetected?.call();
+          }
+        }
+      });
+    });
+  }
+
+  void stopMonitoring() {
+    _accelerometerSubscription?.cancel();
+    _accelerometerSubscription = null;
+    _baselineCaptured = false;
+  }
+}
